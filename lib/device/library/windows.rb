b5e16a9625d180a8e5829d1f729b8f23c87fe190
whiteleaf7 <2nd.leaf@gmail.com>
2013-02-23 12:45:12 +0900

    WIN32APIは古いのでdl/importを使うように変更

--- lib/device/library/windows.rb
+++ lib/device/library/windows.rb
@@ -3,16 +3,26 @@
 # Copyright 2013 whiteleaf. All rights reserved.
 #
 
-require "Win32API"
+require "dl/import"
 require "win32ole"
+
+module WinAPI
+  class InvalidOS < StandardError; end
+  extend DL::Importer
+  begin
+    dlload "kernel32"
+    extern "long GetLogicalDriveStrings(long, void*)"
+  rescue DL::DLError
+    raise InvalidOS, "not Windows"
+  end
+end
 
 module Device::Library
   module Windows
     def get_device_root_dir(volume_name)
-      @@GetLogicalDriveStrings ||= Win32API.new("kernel32", "GetLogicalDriveStrings", "LP", "L")
       @@FileSystemObject ||= WIN32OLE.new("Scripting.FileSystemObject")
       drive_strings = " " * 1000
-      result_len = @@GetLogicalDriveStrings.call(1000, drive_strings)
+      result_len = WinAPI.GetLogicalDriveStrings(1000, drive_strings)
       drives = drive_strings[0, result_len].split("\0")
       drives.each do |drive_letter|
         drive_info = @@FileSystemObject.GetDrive(drive_letter)
