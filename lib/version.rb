9349737924ced4b54b6f3e300b4ffe5667f280f3
whiteleaf7 <2nd.leaf@gmail.com>
2013-02-24 23:00:09 +0900

    add Changes.md

--- lib/version.rb
+++ lib/version.rb
@@ -3,7 +3,7 @@
 # Copyright 2013 whiteleaf. All rights reserved.
 #
 
-Version = "1.0.1"
+Version = "1.0.2"
 
 cv_path = File.expand_path(File.join(File.dirname(__FILE__), "../commitversion"))
 CommitVersion = File.read(cv_path)
