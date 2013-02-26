9ca8a7b9019b4dfba039cda0b9b8f1ed444511cb
whiteleaf7 <2nd.leaf@gmail.com>
2013-02-27 03:00:34 +0900

    @94f88ea24f3e076db1e579ede1845918a0585542 直したつもりがまったく治っていなかったので修正('A`)

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -115,7 +115,7 @@ class Downloader
     when :other
       data = @@database.get_data("title", target)
     when :id
-      data = @@database[target.id]
+      data = @@database[target.to_i]
     end
     return nil unless data
     id = data["id"]
