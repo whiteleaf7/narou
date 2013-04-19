c44867173d7a756a5f83a1a882b3efa2d1cd7c7a
whiteleaf7 <2nd.leaf@gmail.com>
2013-04-19 19:58:13 +0900

    「Ｒｅ：ゼロから始める異世界生活」用の変換設定を追加

---
+++ preset/ncode.syosetu.com/n2267be/converter.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#
+# 対象小説情報
+# タイトル: Ｒｅ：ゼロから始める異世界生活
+# 作者: 鼠色猫
+# URL: http://ncode.syosetu.com/n2267be/
+#
+converter "Ｒｅ：ゼロから始める異世界生活" do
+  def before(io, text_type)
+    super
+    io.string.gsub!(/^[ 　\t]*[※ 　\t]+$/, "※　※　※")
+    io
+  end
+
+  def after(io, text_type)
+    super
+  end
+end
