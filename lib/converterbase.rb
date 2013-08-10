16c2966c8bb6c3eb70c78f84fed600b2532745dd
whiteleaf7 <2nd.leaf@gmail.com>
2013-08-10 10:36:30 +0900

    漢数字をアラビア数字に戻すときに、前後の～を巻き込んで戻すように修正
    例：
    今まで）　三～四〇〇ＭＰ　→　三～４００ＭＰ
    修正後）　三～四〇〇ＭＰ　→　３～４００ＭＰ

--- lib/converterbase.rb
+++ lib/converterbase.rb
@@ -188,10 +188,10 @@ class ConverterBase
   #
   def exception_reconvert_kanji_to_num(data)
     return unless @setting.enable_convert_num_to_kanji
-    data.gsub!(/([Ａ-Ｚａ-ｚ])([#{KANJI_NUM}・]+)/) do   # ｖｅｒ１・０１ のようなパターンも許容する
+    data.gsub!(/([Ａ-Ｚａ-ｚ])([#{KANJI_NUM}・～]+)/) do   # ｖｅｒ１・０１ のようなパターンも許容する
       $1 + $2.tr(KANJI_NUM, "０-９")
     end
-    data.gsub!(/([#{KANJI_NUM}・]+)([Ａ-Ｚａ-ｚ])/) do
+    data.gsub!(/([#{KANJI_NUM}・～]+)([Ａ-Ｚａ-ｚ])/) do
       $1.tr(KANJI_NUM, "０-９") + $2
     end
   end
