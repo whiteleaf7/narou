d9b10dca1fbca11001306dbb6faca28b19fc98af
whiteleaf7 <2nd.leaf@gmail.com>
2013-08-18 17:42:17 +0900

    modify_kana_ni_to_kanji_ni の検出パターンの調整

--- lib/converterbase.rb
+++ lib/converterbase.rb
@@ -835,7 +835,7 @@ class ConverterBase
   # 漢字の二じゃなくて間違えてカタカナのニを使ってるのを校正する
   #
   def modify_kana_ni_to_kanji_ni(data)
-    data.gsub!(/([^ァ-ヶ])ニ([^ァ-ヶ])/) do
+    data.gsub!(/([^ァ-ヶー])ニ([^ァ-ヶー])/) do
       "#{$1}二#{$2}"
     end
   end
