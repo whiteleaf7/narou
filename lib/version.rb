93d781af9a5764a03e878cc41a12e38134f0390b
whiteleaf7 <2nd.leaf@gmail.com>
2014-02-24 18:47:40 +0900

    version bump

--- lib/version.rb
+++ lib/version.rb
@@ -3,7 +3,7 @@
 # Copyright 2013 whiteleaf. All rights reserved.
 #
 
-Version = "1.4.2"
+Version = "1.4.3"
 
 cv_path = File.expand_path(File.join(File.dirname(__FILE__), "../commitversion"))
 if File.exists?(cv_path)
