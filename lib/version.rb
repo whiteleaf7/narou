e9a461d7f18a10de434cce7a561037411cabb247
whiteleaf7 <2nd.leaf@gmail.com>
2014-09-11 19:33:44 +0900

    version 1.6.3

--- lib/version.rb
+++ lib/version.rb
@@ -3,7 +3,7 @@
 # Copyright 2013 whiteleaf. All rights reserved.
 #
 
-Version = "1.6.2"
+Version = "1.6.3"
 
 cv_path = File.expand_path(File.join(File.dirname(__FILE__), "../commitversion"))
 if File.exists?(cv_path)
