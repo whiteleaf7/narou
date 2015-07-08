a74c97e196a1f18b251f4add288ffd75c7c4ff1b
whiteleaf7 <2nd.leaf@gmail.com>
2015-07-09 02:45:15 +0900

    リンクの&をエンティティ化するのを取りやめ
    Kindleでは正しく飛べないし、導入した経緯であるiBooksに関しても、＆amp;があると飛べなくなっていたため

--- lib/converterbase.rb
+++ lib/converterbase.rb
@@ -952,7 +952,7 @@ class ConverterBase
   def rebuild_url(data)
     @url_list.each_with_index do |url, id|
       data.sub!("［＃ＵＲＬ＝#{convert_numbers(id.to_s)}］",
-                "<a href=\"#{Helper.ampersand_to_entity(url)}\">#{url}</a>")
+                "<a href=\"#{url}\">#{url}</a>")
     end
   end
 
