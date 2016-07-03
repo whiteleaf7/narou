da130bcc794e8f2216fc74d71b65a2b51076afcf
whiteleaf7 <2nd.leaf@gmail.com>
2016-07-03 19:50:52 +0900

    narou init をしていないディレクトリでテストが実行出来なかったのを修正

--- spec/support/init_fixtures.rb
+++ spec/support/init_fixtures.rb
@@ -15,9 +15,13 @@ Narou::LOCAL_SETTING_DIR_NAME.replace(".test_dot_narou")
 Narou::GLOBAL_SETTING_DIR_NAME.replace(".test_dot_narousetting")
 Database::ARCHIVE_ROOT_DIR_PATH.replace(".test_novel_data/")
 
+Narou.flush_cache
+
 RSpec.configure do |config|
   config.before :suite do
-    local_dir = Narou.local_setting_dir
+    root_dir = File.expand_path("../..", __dir__)
+    local_dir = File.join(root_dir, ".test_dot_narou")
+    global_dir = File.join(root_dir, ".test_dot_narousetting")
     fixture_narou = File.expand_path("../fixtures/.test_dot_narou", __dir__)
     fixture_novel_data = File.expand_path("../fixtures/.test_novel_data", __dir__)
     if File.directory?(local_dir)
@@ -27,8 +31,8 @@ RSpec.configure do |config|
       FileUtils.rm_r(local_dir, force: true)
       FileUtils.rm_r(Database.archive_root_path, force: true)
     end
-    FileUtils.cp_r(fixture_narou, Narou.get_root_dir)
-    FileUtils.cp_r(fixture_novel_data, Narou.get_root_dir)
+    FileUtils.cp_r(fixture_narou, root_dir)
+    FileUtils.cp_r(fixture_novel_data, root_dir)
     puts "== Copied fixtures version #{fixture_version}"
   end
 end
