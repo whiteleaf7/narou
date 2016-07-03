fcd0efe1980b14def933f41f7cc30715eda8b737
whiteleaf7 <2nd.leaf@gmail.com>
2016-07-03 21:00:05 +0900

    fixtures のコピータイミングを変更

--- spec/support/init_fixtures.rb
+++ spec/support/init_fixtures.rb
@@ -17,22 +17,23 @@ Database::ARCHIVE_ROOT_DIR_PATH.replace(".test_novel_data/")
 
 Narou.flush_cache
 
-RSpec.configure do |config|
-  config.before :suite do
-    root_dir = File.expand_path("../..", __dir__)
-    local_dir = File.join(root_dir, ".test_dot_narou")
-    global_dir = File.join(root_dir, ".test_dot_narousetting")
-    fixture_narou = File.expand_path("../fixtures/.test_dot_narou", __dir__)
-    fixture_novel_data = File.expand_path("../fixtures/.test_novel_data", __dir__)
-    if File.directory?(local_dir)
-      version = Time.parse(File.read(File.join(local_dir, "fixture_version.txt")))
-      fixture_version = Time.parse(File.read(File.join(fixture_narou, "fixture_version.txt")))
-      next if version == fixture_version
-      FileUtils.rm_r(local_dir, force: true)
-      FileUtils.rm_r(Database.archive_root_path, force: true)
-    end
-    FileUtils.cp_r(fixture_narou, root_dir)
-    FileUtils.cp_r(fixture_novel_data, root_dir)
-    puts "== Copied fixtures version #{fixture_version}"
+def install_fixtures
+  root_dir = File.expand_path("../..", __dir__)
+  local_dir = File.join(root_dir, ".test_dot_narou")
+  global_dir = File.join(root_dir, ".test_dot_narousetting")
+  fixture_narou = File.expand_path("../fixtures/.test_dot_narou", __dir__)
+  fixture_novel_data = File.expand_path("../fixtures/.test_novel_data", __dir__)
+
+  if File.directory?(local_dir)
+    version = Time.parse(File.read(File.join(local_dir, "fixture_version.txt")))
+    fixture_version = Time.parse(File.read(File.join(fixture_narou, "fixture_version.txt")))
+    return if version == fixture_version
+    FileUtils.rm_r(local_dir, force: true)
+    FileUtils.rm_r(Database.archive_root_path, force: true)
   end
+  FileUtils.cp_r(fixture_narou, root_dir)
+  FileUtils.cp_r(fixture_novel_data, root_dir)
+  puts "== Copied fixtures version #{fixture_version}"
 end
+
+install_fixtures
