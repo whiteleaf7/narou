069bc20d5ec49589bdf762f5ac9d1dfd0cc6f41f
whiteleaf7 <2nd.leaf@gmail.com>
2016-07-09 21:22:16 +0900

    初期化時点ではパスが解決出来ないので修正

--- spec/support/init_fixtures.rb
+++ spec/support/init_fixtures.rb
@@ -23,13 +23,14 @@ def install_fixtures
   _global_dir = File.join(root_dir, ".test_dot_narousetting")
   fixture_narou = File.expand_path("../fixtures/.test_dot_narou", __dir__)
   fixture_novel_data = File.expand_path("../fixtures/.test_novel_data", __dir__)
+  novel_data_dir = File.join(root_dir, ".test_novel_data")
 
   if File.directory?(local_dir)
     version = Time.parse(File.read(File.join(local_dir, "fixture_version.txt")))
     fixture_version = Time.parse(File.read(File.join(fixture_narou, "fixture_version.txt")))
     return if version == fixture_version
     FileUtils.rm_r(local_dir, force: true)
-    FileUtils.rm_r(Database.archive_root_path, force: true)
+    FileUtils.rm_r(novel_data_dir, force: true)
   end
   FileUtils.cp_r(fixture_narou, root_dir)
   FileUtils.cp_r(fixture_novel_data, root_dir)
