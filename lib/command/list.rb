366c88bf2cf9922c320f655bbdb34d8072d00c17
whiteleaf7 <2nd.leaf@gmail.com>
2016-07-09 18:59:58 +0900

    リファクタリング

--- lib/command/list.rb
+++ lib/command/list.rb
@@ -151,13 +151,15 @@ module Command
       @_view_data_type ||= @options["general-lastup"] ? "general_lastup" : "last_update"
     end
 
+    def now
+      @now ||= Time.now
+    end
+
     def output_list(novels, limit)
-      now = Time.now
-      today = now.strftime("%y/%m/%d")
       filters = @options["filters"] || []
       selected_lines = {}
-      view_date_type_key = view_date_type
       novels.each do |novel|
+        novel_decorator = NovelDecorator.new(novel, self)
         novel_type = novel["novel_type"].to_i
         id = novel["id"]
         frozen = Narou.novel_frozen?(id)
@@ -169,38 +171,7 @@ module Command
         if @options["tags"]
           next unless valid_tags?(novel, @options["tags"])
         end
-        disp_id = ((frozen ? "*" : "") + id.to_s).rjust(4)
-        disp_id = disp_id.sub("*", "<bold><cyan>*</cyan></bold>") if frozen
-        tags = novel["tags"] || []
-        selected_lines[id] = [
-          disp_id,
-          novel[view_date_type_key].strftime("%y/%m/%d").tap { |s|
-            new_arrivals_date = novel["new_arrivals_date"]
-            last_update = novel["last_update"]
-            if new_arrivals_date && new_arrivals_date >= last_update \
-               && new_arrivals_date + ANNOTATION_COLOR_TIME_LIMIT >= now
-              # 新着表示色
-              s.replace "<bold><magenta>#{s}</magenta></bold>"
-            elsif last_update + ANNOTATION_COLOR_TIME_LIMIT >= now
-              # 更新だけあった色
-              s.replace "<bold><green>#{s}</green></bold>"
-            end
-          },
-          @options["kind"] ? NOVEL_TYPE_LABEL[novel_type] : nil,
-          @options["author"] ? novel["author"].escape : nil,
-          @options["site"] ? novel["sitename"].escape : nil,
-          novel["title"].escape + (!@options["kind"] && novel_type == 2 ?
-                           "  <bold><black>(#{NOVEL_TYPE_LABEL[novel_type]})</black></bold>" :
-                           "") +
-                           (tags.include?("end") ? " <bold><black>(完結)</black></bold>" : "") +
-                           (tags.include?("404") ? " <bold><black>(削除)</black></bold>" : ""),
-          @options["url"] ? novel["toc_url"].escape : nil,
-          @options["tags"] || @options["all-tags"] ?
-              tags.empty? ? nil : tags.map{ |tag|
-                color = Tag.get_color(tag)
-                "<bold><#{color}>#{tag.escape}</#{color}></bold>"
-              }.join(",") : nil,
-        ].compact.join(" | ")
+        selected_lines[id] = novel_decorator.decorate_line
       end
       if @options["grep"]
         @options["grep"].each do |search_word|
@@ -244,6 +215,105 @@ module Command
       end
       database_values.reverse! if @options["reverse"]
       output_list(database_values, limit)
+    end
+
+    class NovelDecorator
+      attr_accessor(*%i(id frozen novel_type novel options parent))
+
+      def initialize(novel, parent)
+        self.id = novel["id"]
+        self.frozen = Narou.novel_frozen?(id)
+        self.novel_type = novel["novel_type"].to_i
+        self.novel = novel
+        self.options = parent.instance_variable_get(:@options)
+        self.parent = parent
+      end
+
+      def decorate_id
+        disp_id = ((frozen ? "*" : "") + id.to_s).rjust(4)
+        if frozen
+          disp_id.sub("*", "<bold><cyan>*</cyan></bold>")
+        else
+          disp_id
+        end
+      end
+
+      def decorate_date
+        date = novel[parent.view_date_type].strftime("%y/%m/%d")
+        new_arrivals_date = novel["new_arrivals_date"]
+        last_update = novel["last_update"]
+        if new_arrivals_date && new_arrivals_date >= last_update &&
+           new_arrivals_date + ANNOTATION_COLOR_TIME_LIMIT >= now
+          # 新着表示色
+          "<bold><magenta>#{date}</magenta></bold>"
+        elsif last_update + ANNOTATION_COLOR_TIME_LIMIT >= now
+          # 更新だけあった色
+          "<bold><green>#{date}</green></bold>"
+        else
+          date
+        end
+      end
+
+      def decorate_kind
+        options["kind"] ? NOVEL_TYPE_LABEL[novel_type] : nil
+      end
+
+      def decorate_author
+        options["author"] ? novel["author"].escape : nil
+      end
+
+      def decorate_site
+        options["site"] ? novel["sitename"].escape : nil
+      end
+
+      def decorate_title
+        if !options["kind"] && novel_type == 2
+          type = " <bold><black>(#{NOVEL_TYPE_LABEL[@novel_type]})</black></bold>"
+        end
+        the_end = "<bold><black>(完結)</black></bold>" if tags.include?("end")
+        delete = "<bold><black>(削除)</black></bold>" if tags.include?("404")
+        [
+          novel["title"].escape,
+          type,
+          the_end,
+          delete
+        ].compact.join(" ")
+      end
+
+      def decorate_url
+        options["url"] ? novel["toc_url"].escape : nil
+      end
+
+      def decorate_tags
+        return nil unless options["tags"] || options["all-tags"]
+        tags.empty? ? nil : tags.map { |tag|
+          color = Tag.get_color(tag)
+          "<bold><#{color}>#{tag.escape}</#{color}></bold>"
+        }.join(",")
+      end
+
+      def decorate_line
+        [
+          decorate_id,
+          decorate_date,
+          decorate_kind,
+          decorate_author,
+          decorate_site,
+          decorate_title,
+          decorate_url,
+          decorate_tags
+        ].compact.join(" | ")
+      end
+
+      private
+
+      def tags
+        @__tags ||= novel["tags"] || []
+      end
+
+      def now
+        parent.now
+      end
     end
   end
 end
