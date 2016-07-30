22d989da9ab550b6eda0498f8f9bc03e2a988efd
Kubo Takehiro <kubo@jiubao.org>
2016-07-30 15:10:24 +0900

    Ubuntu 16.04 で send コマンドが動くように修正

--- lib/device/library/linux.rb
+++ lib/device/library/linux.rb
@@ -2,11 +2,14 @@
 #
 # Copyright 2013 whiteleaf. All rights reserved.
 #
+require 'etc'
 
 module Device::Library
   module Linux
+    @@mount_roots = %w(/media /mnt)
+    @@mount_roots << "/media/" + (Etc.getlogin || Etc.getpwuid.name)
     def get_device_root_dir(volume_name)
-      %w(/media /mnt).each do |mount_root|
+      @@mount_roots.each do |mount_root|
         path = File.join(mount_root, volume_name)
         if File.directory?(path)
           return path
