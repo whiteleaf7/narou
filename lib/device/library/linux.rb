8d6973349c490dd0d0b520c277f76eff2f6a8d51
Kubo Takehiro <kubo@jiubao.org>
2016-08-01 22:43:07 +0900

    Ubuntu 以外の udisks2 を使っている Linux でも send コマンドが動くように修正

--- lib/device/library/linux.rb
+++ lib/device/library/linux.rb
@@ -6,8 +6,10 @@ require "etc"
 
 module Device::Library
   module Linux
+    username = (Etc.getlogin || Etc.getpwuid.name)
     @@mount_roots = %w(/media /mnt)
-    @@mount_roots << "/media/" + (Etc.getlogin || Etc.getpwuid.name)
+    @@mount_roots << "/run/media/" + username # udisks2 による自動マウント(デフォルトの場所)
+    @@mount_roots << "/media/" + username # udisks2 による自動マウント(Ubuntuの場合)
     def get_device_root_dir(volume_name)
       @@mount_roots.each do |mount_root|
         path = File.join(mount_root, volume_name)
