202379a78562871635b4d82ba53a34b14ea23534
whiteleaf7 <2nd.leaf@gmail.com>
2016-09-11 20:35:45 +0900

    サブタイトルの中では全角ダッシュの画像化をやめる

--- lib/converterbase.rb
+++ lib/converterbase.rb
@@ -1416,6 +1416,10 @@ class ConverterBase
 
   def double_dash_to_image(text, output_text_dir)
     return text unless @setting.enable_double_dash_to_image
+    # サブタイトルの中の場合は無視する
+    # （サブタイトルは文字を大きくしているので、画像の位置がずれてしまうため）
+    return text if @text_type == "subtitle"
+
     begin
       # AozoraEpub3 は相対パスじゃないとエラーになるので相対パスに変換
       dash_paths = dash_image_relative_paths(Narou.get_preset_dir, output_text_dir)
