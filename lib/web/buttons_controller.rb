54cbe2f37273c1bcd6782d6c53ec225bbc243e85
whiteleaf7 <2nd.leaf@gmail.com>
2016-11-19 19:23:56 +0900

    save

--- lib/web/buttons_controller.rb
+++ lib/web/buttons_controller.rb
@@ -1,41 +1,24 @@
-class CustomButton
-  attr_accessor :name, :html_code, :js_code
-
-  def self.all
-  end
-
-  def initialize(name)
-    self.name = name
-    load_resources
-  end
-
-  private
-
-  def load_resources
-    self.html_code = File.read(html_code_path)
-    self.js_code = File.read(js_code_path)
-  end
-
-  def buttons_dir
-    File.join(Narou.get_misc_dir, "buttons")
-  end
-
-  def html_code_path
-    File.join(buttons_dir, "#{@name}.html")
-  end
-
-  def js_code_path
-    File.join(buttons_dir, "#{@name}.js")
-  end
-end
-
 class ButtonsController < Narou::ApplicationController
+  def load_button_resources
+    dir = ButtonsController.buttons_dir
+    @html_code = load_buttons_resource("buttons.html")
+    @js_code = load_buttons_resource("buttons.js")
+  end
+
+  def load_buttons_resource(file)
+    Helper::CacheLoader.load(File.join(Narou.get_misc_dir, "buttons", file))
+  rescue Errno::ENOENT
+    ""
+  end
+
+  # Routing ===============================================================
+
   before do
     @title = "操作ボタンの編集"
   end
 
   get "/buttons" do
-    @buttons = []
+    load_button_resources
     haml :"buttons/index"
   end
 end
