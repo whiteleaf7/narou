66d922778562b1484a007de600cdd42c9e174fe0
whiteleaf7 <2nd.leaf@gmail.com>
2017-08-13 14:59:08 +0900

    設定をグループ化して取得できるようにして設定値を取得しやすく出来るように

--- lib/web/appserver.rb
+++ lib/web/appserver.rb
@@ -272,12 +272,12 @@ class Narou::AppServer < Sinatra::Base
   # サーバーの認証の設定
   # とりあえずDigest認証のみ
   def setup_server_authentication
-    setting = Inventory.load("global_setting", :global)
-    return unless setting["server-digest-auth.enable"]
+    auth = Inventory.load("global_setting", :global).group("server-digest-auth")
+    return unless auth.enable
 
-    user = setting["server-digest-auth.user"]
-    hashed = setting["server-digest-auth.hashed-password"]
-    passwd = hashed || setting["server-digest-auth.password"]
+    user = auth.user
+    hashed = auth.hashed_password
+    passwd = hashed || auth.password
 
     self.class.class_exec do
       use Rack::Auth::Digest::MD5, { realm: "narou.rb", opaque: "", passwords_hashed: hashed } do |username|
--- lib/web/settingmessages.rb
+++ lib/web/settingmessages.rb
@@ -20,7 +20,8 @@ module Narou
     "no-color" => "コンソールのカラー表示を無効にする\n※要WEB UIサーバ再起動",
     "economy" => "容量節約に関する設定",
     "send.without-freeze" => "一括送信時に凍結された小説は対象外にする。（個別送信時は凍結済みでも送信可能）",
-    "server-digest-auth.use" => "WEBサーバでDigest認証を使用するかどうか\n※要WEB UIサーバ再起動",
+    "server-digest-auth.enable" => "WEBサーバでDigest認証を使用するかどうか\n" \
+                                   "※digest-auth関連の設定を変更した場合WEB UIサーバの再起動が必要",
     "server-digest-auth.hashed-password" => "WEBサーバのDigest認証のパスワードを、Realmを\"narou.rb\"としてハッシュにしたもの。\nhttps://tgws.plus/app/digest/ などで生成できる",
   }
 end
