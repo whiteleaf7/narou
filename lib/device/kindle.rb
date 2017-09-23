a850b22caf2f8c5b7954bc7cd5eb1e9416261f12
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-09-23 22:04:14 +0900

    Revert modification in lib/device/kindle.rb since this is a place tp specify somthine what necessary.  Slice_size is an option, so it should not be written here.

--- lib/device/kindle.rb
+++ lib/device/kindle.rb
@@ -15,7 +15,6 @@ module Device::Kindle
 
   RELATED_VARIABLES = {
     "default.enable_half_indent_bracket" => true,
-    "default.slice_size" => 500,
   }
 
   include Device::BackupBookmarkUtility
