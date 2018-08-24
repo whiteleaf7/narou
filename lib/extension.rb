3e4f9b1be527f87fb7d01310a95249efc1d52d72
whiteleaf7 <2nd.leaf@gmail.com>
2018-08-25 00:54:09 +0900

    クラッシュ防止書き込みできてなかったので修正／mode指定時はsuper
    .yaml でも別名で保存してからリネームする

--- lib/extension.rb
+++ lib/extension.rb
@@ -18,22 +18,19 @@ end
 #
 require "securerandom"
 
-def File.write(path, string, *options)
+def File.write(path, string, *options, mode: nil)
+  return super if mode
+
   dirpath = File.dirname(path)
-  backup = false
-  temp_path =
-    if File.extname(path) == ".yaml" && File.basename(dirpath) != Downloader::SECTION_SAVE_DIR_NAME
-      backup = true
-      "#{path}.backup"
-    else
-      File.join(dirpath, SecureRandom.hex(15))
-    end
-
-  super(temp_path, string, *options)
-
-  if backup
-    super(path, string, *options)
-  else
-    File.rename(temp_path, path)
+  temp_path = File.join(dirpath, SecureRandom.hex(15))
+  if File.extname(path) == ".yaml" && File.basename(dirpath) != Downloader::SECTION_SAVE_DIR_NAME
+    backup = "#{path}.backup"
   end
+
+  res = super(temp_path, string, *options)
+  if backup
+    super(backup, string, *options)
+  end
+  File.rename(temp_path, path)
+  res
 end
