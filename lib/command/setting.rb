57227bbb1633554cd1774ecfb1947ccb0819c4e7
whiteleaf7 <2nd.leaf@gmail.com>
2018-09-15 01:43:19 +0900

    narou init 前のフォルダで実行するとクラッシュしてた

--- lib/command/setting.rb
+++ lib/command/setting.rb
@@ -371,7 +371,7 @@ module Command
           type: :string
         },
         "logging" => {
-          help: "ログの保存を有効にする。保存場所は#{Narou.log_dir.basename}フォルダ。concurrencyが有効な場合、変換ログだけ別ファイルに出力される",
+          help: "ログの保存を有効にする。保存場所は#{Narou.log_dir&.basename}フォルダ。concurrencyが有効な場合、変換ログだけ別ファイルに出力される",
           type: :boolean,
           tab: :general
         },
