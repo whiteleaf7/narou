7ad3af9979a44aabee9a81c4da64fb2611e0f961
Yukihiro Marui <marui@g.ecc.u-tokyo.ac.jp>
2020-11-28 00:28:52 +0900

    Fix targets to target

--- lib/web/appserver.rb
+++ lib/web/appserver.rb
@@ -835,10 +835,10 @@ class Narou::AppServer < Sinatra::Base
 
   # ダウンロード登録すると同時にグレーのボタン画像を返す
   get "/api/download4ssl" do
-    targets = params["targets"] or error("need a parameter: `targets'")
+    target = params["target"] or error("need a parameter: `target'")
     opt_mail = "--mail" if query_to_boolean(params["mail"])
     Narou::WebWorker.push do
-      CommandLine.run!("download", targets, opt_mail)
+      CommandLine.run!("download", target, opt_mail)
       @@push_server.send_all(:"table.reload")
     end
     redirect "/resources/images/dl_button1.gif"
